e14f4df63946daf9c4f7ddf2bd831a1c0901321a
alexvanhell <alejandrodnl.cv@hotmail.com>
2018-11-25 19:58:19 -0600

    Finishing example

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const emailSender = require('./lib/email-sender');
 
-emailSender.sendMail();
+async function main() {
+    let email = {
+        from: '"Test user" <foo@example.com>',
+        to: 'bar@example.com',
+        subject: 'Subject test',
+        html: '<b>Hola mundo</b>'
+    }
+
+    let mailSent = {};
+
+    try {
+        mailSent = await emailSender.sendMail(email);
+    } catch (err) {
+        console.log(err);
+    }
+
+    console.log('Envio exitoso:', mailSent);
+}
+
+main();
---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MAIL_CONFIG: {
+        HOST: 'smtp.mailtrap.io',
+        PORT: 2525,
+        SECURE: false,
+        USER: '24c837fe7ddda2',
+        PASSWORD: 'c9cc949880b5e6',
+    }
+}
